d5f59720d72b4bfb8834b75be377e832166422f6
John Koumentis <jokoum92@gmail.com>
2022-07-05 23:44:55 +0300

    modified package dir

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     author_email="jokoum92@gmail.com",
     license="MIT",
     packages=['pysgt','pysgt.utils'],
+    package_dir = {"": "pysgt"},
     install_requires=["numpy>=1.20.2", "scipy>=1.6.2", "pandas>=1.3.3", "scikit-learn>=0.24.2"],
     zip_safe=False
 )
